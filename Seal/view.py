bc9269875520706c99a6f26d5cb931a0a8e66841
root <root@localhost.localdomain>
2018-05-16 21:48:53 -0400

    add new test Model

--- Seal/view.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def hello(request):
-	context={}
-	context["hello"]="Hello Flex"
-	return render(request, "hello.html", context)
